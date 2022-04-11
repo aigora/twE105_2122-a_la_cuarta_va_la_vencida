8ee5d18114f9e3a29ed64e009e66ee6ca4473b87
xrubenruizx <61049993+xrubenruizx@users.noreply.github.com>
2022-04-11 18:16:47 +0200

    Update main.c

--- juego/main.c
+++ juego/main.c
@@ -9,15 +9,16 @@ int main(){
   
   switch (sel) {
   	case 1:
-  		printf("Empieza el juego\n"); //Esto es temporal, aquí iría el código para iniciar el juego
+  		printf("Empieza el juego\n"); //Esto es temporal, aquÃ­ irÃ­a el cÃ³digo para iniciar el juego
   	
 	case 2:
-		printf("Instrucciones\n"); //El programa lee un archivo txt e imprime en pantalla lo leído. 
+		printf("Instrucciones\n"); //El programa lee un archivo txt e imprime en pantalla lo leÃ­do. 
 	
-	case 3:
-          printf("Partidas Anteriores\n"); //El programa lee un archivo txt con infromacion sobre partidas anteriores lmacenadas en la memoria.
-    case 4:
-          printf("Tabla de mejores jugadores\n"); // El programa lee un archivo txt con los nombres de los jugadores y sus victorias.
+	case 3:	
+       		printf("Partidas Anteriores\n"); //El programa lee un archivo txt con infromacion sobre partidas anteriores lmacenadas en la memoria.
+		  
+    	case 4:
+		printf("Tabla de mejores jugadores\n"); // El programa lee un archivo txt con los nombres de los jugadores y sus victorias.
   }
   
   return 0;
