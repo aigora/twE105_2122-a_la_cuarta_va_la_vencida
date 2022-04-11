cea8d281ab2236245c90ca0165a3f295a869b0b1
Joshgv25 <61049984+Joshgv25@users.noreply.github.com>
2022-04-11 17:17:08 +0200

    Update main.c

--- juego/main.c
+++ juego/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main(){
+  int sel;
   
+  
+  printf("Bienvenido! Seleccione una de las siguientes opciones:\n1. Jugar\n2. Instrucciones\n3. Partidas anteriores\n4. Ranking de mejores jugadores");
+  scanf("%i", sel);
+  
+  switch (sel) {
+  	case '1': 
+  		printf("Empieza el juego\n"); //Esto es temporal, aquí iría el código para iniciar el juego
+  	
+	case '2':
+		printf("Instrucciones\n"); //El programa lee un archivo txt e imprime en pantalla lo leído. 
+	
+	case '3':
+				
+  }
   
   return 0;
 }
