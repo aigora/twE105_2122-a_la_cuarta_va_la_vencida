c2c55516d38df7f0721cac14425760231d10bebe
xrubenruizx <ruben.ruiz@alumnos.upm.es>
2022-04-11 19:03:49 +0200

    Update main.c

--- juego/main.c
+++ juego/main.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 
-int main(){
-  int sel;
-  
-  
-  printf("Bienvenido! Seleccione una de las siguientes opciones:\n1. Jugar\n2. Instrucciones\n3. Partidas anteriores\n4. Ranking de mejores jugadores");
-  scanf("%i", sel);
-  
-  switch (sel) {
-  	case 1:
-  		printf("Empieza el juego\n"); //Esto es temporal, aquí iría el código para iniciar el juego
-  	
-	case 2:
-		printf("Instrucciones\n"); //El programa lee un archivo txt e imprime en pantalla lo leído. 
-	
-	case 3:
-          printf("Partidas Anteriores\n"); //El programa lee un archivo txt con infromacion sobre partidas anteriores lmacenadas en la memoria.
-    case 4:
-          printf("Tabla de mejores jugadores\n"); // El programa lee un archivo txt con los nombres de los jugadores y sus victorias.
-  }
-  
-  return 0;
+int main(int argc, const char * argv[]) {
+    
+    int sel, modo, color, temp;
+    char jugador1[20], jugador2[20];
+    
+    printf("Bienvenido! Elige una de las opciones\n");
+    printf("1. Nueva Partida\n2. Cargar Partida Anterior\n 3. Instrucciones\n 4. Ranking\n5. Salir");
+    scanf("%i", &sel);
+    
+    do{
+    switch(sel){
+            
+        case 1:
+            do{
+            printf("Elige modo de juego:\n");
+            printf("1. Humano vs. Humano\n2. Humano vs. Maquina\n");
+            scanf("%i", &modo);
+            } while (modo !=  )
+            if(modo==1){
+                printf("Ingrese el nombre del jugador 1:\n");
+                fgets(jugador1, 20, stdin);
+                
+                printf("ÀQue color sera %s?\n", jugador1);
+                printf("1. Blancas\n2. Negras\n");
+                scanf("%i", &color);
+                if(color == 1){
+                    printf("De acuerdo, la maquina sera el color negro\n");
+                } else if(color == 2){
+                    printf("De acuerdo, la maquina sera el color blanco\n");
+                }
+                
+                printf("ÀDesea jugar con limite de tiempo?\n");
+                printf("1. Si\n2. No\n");
+                scanf("%i", &temp);
+                
+                
+            }
+    }
+    } while(sel <1 && sel>5)
+    
+    return 0;
 }
-  
