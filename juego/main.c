1c37e140b567eb75b2c9b5c9d7f42330816b9bd8
xrubenruizx <ruben.ruiz@alumnos.upm.es>
2022-04-11 18:14:57 +0200

    Update main.c

--- juego/main.c
+++ juego/main.c
@@ -8,14 +8,16 @@ int main(){
   scanf("%i", sel);
   
   switch (sel) {
-  	case '1': 
+  	case 1:
   		printf("Empieza el juego\n"); //Esto es temporal, aquí iría el código para iniciar el juego
   	
-	case '2':
+	case 2:
 		printf("Instrucciones\n"); //El programa lee un archivo txt e imprime en pantalla lo leído. 
 	
-	case '3':
-				
+	case 3:
+          printf("Partidas Anteriores\n"); //El programa lee un archivo txt con infromacion sobre partidas anteriores lmacenadas en la memoria.
+    case 4:
+          printf("Tabla de mejores jugadores\n"); // El programa lee un archivo txt con los nombres de los jugadores y sus victorias.
   }
   
   return 0;
